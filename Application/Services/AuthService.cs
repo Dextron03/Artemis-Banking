9f7ca2095061f68ec10962c4f85619821c808ebc
kev23453 <kevingarcia22120514@gmail.com>
2026-03-13 17:18:23 -0400

    Adding authentication module

---
+++ Application/Services/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.Interfaces;
+using Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthService(SignInManager<AppUser> signInManager,
+                           UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> LoginAsync(LoginDto dto)
+        {
+            var user = await _userManager.FindByNameAsync(dto.Email);
+
+            if (user == null)
+                return false;
+
+            if (!user.IsActive)
+                throw new Exception("Cuenta inactiva");
+
+            var result = await _signInManager.PasswordSignInAsync(
+                dto.Email,
+                dto.Password,
+                false,
+                false
+            );
+
+            return result.Succeeded;
+        }
+    }
+}
