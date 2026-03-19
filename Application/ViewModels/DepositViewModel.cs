4cfb56b86d0b4e8317ef49d0d7ebb4c2ad6f8a28
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-03-19 18:49:02 -0400

    Cree algunos viewmodels para el cajero

---
+++ Application/ViewModels/DepositViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class DepositViewModel
+    {
+        [Required(ErrorMessage = "El numero de cuenta es obligatorio")]
+        public string AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "El monto es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+        public decimal Amount { get; set; }
+    
+    }
+}
